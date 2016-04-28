cd742900cd81071a9544ee01f0385e4094ff5e46
Gabe Kessler <gabe@gabesstudio.com>
2016-04-28 08:35:42 -0400

    initial commit

---
+++ mdeverywhere_mobile/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  mdeverywhere_mobile
+//
+//  Created by Gabe Kessler on 4/27/16.
+//  Copyright © 2016 Gabe Kessler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, NSXMLParserDelegate
+{
+    @IBOutlet var tbData : UITableView?
+    
+    var parser = NSXMLParser()
+    var posts = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var title1 = NSMutableString()
+    var date = NSMutableString()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.beginParsing()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func beginParsing()
+    {
+        posts = []
+        parser = NSXMLParser(contentsOfURL:(NSURL(string:"http://images.apple.com/main/rss/hotnews/hotnews.rss"))!)!
+        parser.delegate = self
+        parser.parse()
+        
+        tbData!.reloadData()
+    }
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String])
+    {
+        element = elementName
+        if (elementName as NSString).isEqualToString("item")
+        {
+            elements = NSMutableDictionary()
+            elements = [:]
+            title1 = NSMutableString()
+            title1 = ""
+            date = NSMutableString()
+            date = ""
+        }
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
+    {
+        if (elementName as NSString).isEqualToString("item") {
+            if !title1.isEqual(nil) {
+                elements.setObject(title1, forKey: "title")
+            }
+            if !date.isEqual(nil) {
+                elements.setObject(date, forKey: "date")
+            }
+            
+            posts.addObject(elements)
+        }
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String)
+    {
+        if element.isEqualToString("title") {
+            title1.appendString(string)
+        } else if element.isEqualToString("pubDate") {
+            date.appendString(string)
+        }
+    }
+    
+    //Tableview Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+        
+        if(cell.isEqual(NSNull)) {
+            cell = NSBundle.mainBundle().loadNibNamed("Cell", owner: self, options: nil)[0] as! UITableViewCell;
+        }
+        
+        cell.textLabel?.text = posts.objectAtIndex(indexPath.row).valueForKey("title") as! NSString as String
+        cell.detailTextLabel?.text = posts.objectAtIndex(indexPath.row).valueForKey("date") as! NSString as String
+        
+        return cell as UITableViewCell
+    }
+}
+
